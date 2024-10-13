2c35ce0f68d36066008af90a119da3afdd84720c
Lex <79052688528@yandex.ru>
2024-10-13 16:58:26 +0300

    13.10

---
+++ module2_2.py
@@ -0,0 +1,6 @@
+first_=int(input('Ведите первое число-'))
+second_=int(input('Введите второе число-'))
+third_=int(input('Введите третье число-'))
+if first_==second_ and second_==third_: print(3)
+elif first_==second_ or second_==third_ or first_==third_:print(2)
+else:print(0)
