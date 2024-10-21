13b8a4b6e8615a9674d46918e8af4a60724f591f
Lex <79052688528@yandex.ru>
2024-10-21 17:46:10 +0300

    13.10

--- module2_3.py
+++ module2_3.py
@@ -1,11 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 in_x = 0
-while in_x <= len(my_list):
-    if in_x < len(my_list) and my_list[in_x] >= 0:
+while in_x < len(my_list):
+    if in_x < len(my_list) and my_list[in_x] > 0:
         print(my_list[in_x])
-        in_x += 1
-        continue
-    else:
-        print('Вывод по поставленным условиям завершен')
-        break
+    in_x += 1
+print('Вывод по поставленным условиям завершен')
 
